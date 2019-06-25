8c7ab144e65e2c306cac1b1716d75a66d0877fc1
Yukios7 <fabioyukio123456@gmail.com>
2019-06-25 11:38:33 -0300

    f

---
+++ src/F/F.java
@@ -0,0 +1,18 @@
+package F;
+
+public class F {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char a = 'a';
+		int i = 0;
+		float f = 10.3f;
+		double d = 10;
+		
+		System.out.println("a = " + a);
+		System.out.println("i = " + i);
+		System.out.println("f = " + f);
+		System.out.println("d = " + d);
+	}
+
+}
